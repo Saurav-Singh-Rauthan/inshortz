5ccef62f9089d498006da4d1c96239ac738c8baf
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-08-03 12:33:17 +0530

    navbar desk

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Styles from './Navbar.module.css';
+import Styles from "./Navbar.module.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FindInPageIcon from '@mui/icons-material/FindInPage';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 const Navbar = (props) => {
-  return(
+  return (
     <div className={Styles.cont}>
-      {props.children}
+      <p>INSHORTZ</p>
+      <div className={Styles.iconCont}>
+        <div className={Styles.icon}>
+          <AddCircleIcon />
+        </div>
+        <div className={Styles.icon}>
+          <FindInPageIcon />
+        </div>
+        <div className={Styles.icon}>
+          <AccountCircleIcon />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
