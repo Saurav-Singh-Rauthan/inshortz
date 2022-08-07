de75ab0ce30c5dc14d0efd2d35730e6a4d9b1461
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-08-07 13:50:00 +0530

    added helper text for shorts creator

--- src/Components/Pages/AddRecord/AddRecord.js
+++ src/Components/Pages/AddRecord/AddRecord.js
@@ -85,13 +85,14 @@ const AddRecord = (props) => {
             <MenuItem value={"vid"}>Video</MenuItem>
           </Select>
         </FormControl>
-
+        <p className={Styles.helper}>Select type for content (image/video)</p>
         <TextField
           id="outlined-basic"
           label="Link"
           required
           variant="outlined"
           onChange={(event) => valueChangeHandler(event, "link")}
+          helperText={"Link for the image/video file"}
         />
         <TextField
           id="outlined-basic"
@@ -99,6 +100,11 @@ const AddRecord = (props) => {
           required
           variant="outlined"
           onChange={(event) => valueChangeHandler(event, "title")}
+          helperText={`Title for the short (${
+            shortData.title !== null
+              ? shortData.title.trim().split(" ").length
+              : 0
+          } / 20 words)`}
         />
         <TextareaAutosize
           placeholder="Content *"
@@ -106,6 +112,11 @@ const AddRecord = (props) => {
           className={Styles.textarea}
           onChange={(event) => valueChangeHandler(event, "content")}
         />
+        <p className={Styles.helper}>{`Content for the short (${
+          shortData.content !== null
+            ? shortData.content.trim().split(" ").length
+            : 0
+        } / 100 words)`}</p>
         <TextField
           id="outlined-basic"
           label="Tags"
@@ -113,6 +124,7 @@ const AddRecord = (props) => {
           variant="outlined"
           placeholder="India Festival Fun"
           onChange={(event) => valueChangeHandler(event, "tags")}
+          helperText={`Tags for the short (${shortData.tags.length}/5)`}
         />
         <FormControlLabel
           labelPlacement="start"
